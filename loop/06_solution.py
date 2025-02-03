03de77258fbe5b84b6d86d9615e9e35a2cd76ad0
tejash <tejashyadavinfo@gmail.com>
2025-02-03 22:48:53 +0530

    factorial method

---
+++ loop/06_solution.py
@@ -0,0 +1,18 @@
+# number = int(input('Number: '))
+
+# count = 1
+
+# for n in range(1, number + 1):
+#     count *= n
+# print(f' Factorial of {number} is {count}')
+
+
+# number = int(input('Number: '))
+
+# factorials = 1
+# original_number = number
+
+# while number > 0:
+#     factorials *= number 
+#     number -= 1
+# print(f'Factorial of {original_number} is {factorials}')
