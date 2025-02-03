68e09fe1091f6a8813679ebe8867eb4c43252e0b
tejash <tejashyadavinfo@gmail.com>
2025-02-03 22:49:28 +0530

    factorial value add

--- loop/06_solution.py
+++ loop/06_solution.py
@@ -1,18 +1,18 @@
-# number = int(input('Number: '))
+number = int(input('Number: '))
 
-# count = 1
+count = 1
 
-# for n in range(1, number + 1):
-#     count *= n
-# print(f' Factorial of {number} is {count}')
+for n in range(1, number + 1):
+    count *= n
+print(f' Factorial of {number} is {count}')
 
 
-# number = int(input('Number: '))
+number = int(input('Number: '))
 
-# factorials = 1
-# original_number = number
+factorials = 1
+original_number = number
 
-# while number > 0:
-#     factorials *= number 
-#     number -= 1
-# print(f'Factorial of {original_number} is {factorials}')
+while number > 0:
+    factorials *= number 
+    number -= 1
+print(f'Factorial of {original_number} is {factorials}')
