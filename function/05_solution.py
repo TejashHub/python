a6624c158c5edc47fd36d0c260cffb2fe7fb8831
tejash <tejashyadavinfo@gmail.com>
2025-02-04 15:40:59 +0530

    greeting message

---
+++ function/05_solution.py
@@ -0,0 +1,9 @@
+def greet(name = 'Tejash'):
+    greet = f'Good Morning {name}'
+    return greet
+
+print(greet('Payal'))
+print(greet('Sneha'))
+print(greet('Deepika'))
+print(greet('Ankita'))
+print(greet()) 
